62d9d4075be62dd9a56234456c872d4eb8f84c67
Nelli Butenko <butenkonelly93@gmail.com>
2021-04-29 01:34:13 +0000

    Sanitize code to prevent SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,19 +9,18 @@ const pool = new Pool({
 
 const cohort = process.argv.slice(2)[0];
 const limit = process.argv.slice(2)[1];
+const values = [`%${cohort}%`, limit]; // save values separately to avoid SQL injections
 
+// use $1, $2 etc signs to sanitize the code
 pool.query(`
 SELECT students.id, students.name as student, cohorts.name as cohort
 FROM students
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${cohort}%'
-LIMIT ${limit};
-`)
+WHERE cohorts.name LIKE $1 
+LIMIT $2
+`, values) // pass them as a second parameter
 
-  // .then(res => {
-  //   console.log(res);
-  // })
-  // .catch(err => console.error('query error', err.stack));
+
 
   .then(res => {
     res.rows.forEach(result => {
